e8ef570f58814cfad400c4efeda50b11944e1e46
Vasco Xavier <vliscoxavier06@gmail.com>
2020-02-25 17:40:31 +0200

    configure api urls in main app

--- callcenter/urls.py
+++ callcenter/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include
+from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
+    path('api/v1/', include('callcenter.api_urls')),
     path('api/v1/o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('admin/', admin.site.urls),
 ]
