603f50580c1ef63ead24bf0f496928b2ab65ddb0
Vasco Xavier <vliscoxavier06@gmail.com>
2020-02-27 16:23:26 +0200

    change database type fields from text to char

--- case_manager/models.py
+++ case_manager/models.py
@@ -10,35 +10,35 @@ from location_management.models import Province
 
 # Create your models here.
 class CasePriority(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Gender(models.Model):
-    name = models.TextField(max_length=25)
+    name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
 
 
 class Programme(models.Model):
-    name = models.TextField(max_length=250)
+    name = models.CharField(max_length=250)
 
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
 class SubCategory(models.Model):
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_category')
 
     def __str__(self):
@@ -46,21 +46,21 @@ class SubCategory(models.Model):
 
 
 class SubCategoryIssue(models.Model):
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
 class ResolutionCategory(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class ResolutionSubCategory(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
     resolution_category = models.ForeignKey(ResolutionCategory, on_delete=models.CASCADE, related_name='resolution_subcategory')
 
     def __str__(self):
@@ -68,28 +68,28 @@ class ResolutionSubCategory(models.Model):
 
 
 class HowDoYouHearAboutUs(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class HowWouldYouLikeToBeContacted(models.Model):
-    name = models.TextField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class CustomerSatisfaction(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class TaskStatus(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -97,31 +97,31 @@ class TaskStatus(models.Model):
 
 class Contactor(models.Model):
 
-    age = models.TextField(blank=True, default='')
-    alternative_number = models.TextField(default='', blank=True, null=True)
+    age = models.CharField(blank=True, default='', max_length=50)
+    alternative_number = models.CharField(default='', max_length=25, blank=True, null=True)
     community = models.ForeignKey(Community, on_delete=models.SET_NULL, related_name='contactor', null=True, blank=True)
-    contact = models.TextField(max_length=100)
+    contact = models.CharField(max_length=100)
     district = models.ForeignKey(District, on_delete=models.SET_NULL, related_name='contactor', null=True, blank=True)
-    fdp = models.TextField(max_length=200, default='', null=True, blank=True)
-    full_name = models.TextField(max_length=300)
+    fdp = models.CharField(max_length=200, default='', null=True, blank=True)
+    full_name = models.CharField(max_length=300)
     gender = models.ForeignKey(Gender, on_delete=models.CASCADE, related_name='contactor')
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, related_name='contactor', null=True, blank=True)
     province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='contactor')
 
 
 class ReferallEntity(models.Model):
-    name = models.TextField(unique=True, max_length=200)
+    name = models.CharField(unique=True, max_length=200)
     email = models.EmailField()
-    contact = models.TextField(default='', blank=True, null=True)
+    contact = models.CharField(default='', blank=True, null=True, max_length=25)
 
 
 class Case(models.Model):
 
     call_note = models.TextField(max_length=1000)
-    camp = models.TextField(choices=[('Y','YES'),('N','NO')])
+    camp = models.CharField(choices=[('Y','YES'),('N','NO')], max_length=25)
     caller_not_reached_for_feedback = models.BooleanField(default=True, blank=True, null=True)
     case_closed = models.BooleanField(default=False)
-    case_id = models.TextField(max_length=20, unique=True)
+    case_id = models.CharField(max_length=20, unique=True)
     case_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     case_priority = models.ForeignKey(CasePriority, on_delete=models.SET_NULL, related_name='cases', null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='cases', null=True)
@@ -132,14 +132,14 @@ class Case(models.Model):
     closed_at = models.DateTimeField(auto_now=False, null=True, blank=True)
     how_knows_us = models.ForeignKey(HowDoYouHearAboutUs, on_delete=models.SET_NULL ,related_name='cases', null=True)
     humanitarion_actor = models.TextField(max_length=200)
-    local_leader = models.TextField(max_length=200)
-    other_vulnerabilites = models.TextField(max_length=200, null=True, blank=True)
-    resettlement_name = models.TextField(max_length=200, null=True, blank=True)
+    local_leader = models.CharField(max_length=200)
+    other_vulnerabilites = models.CharField(max_length=200, null=True, blank=True)
+    resettlement_name = models.CharField(max_length=200, null=True, blank=True)
     resolution_callback = models.BooleanField(default=False)
     soluction = models.TextField(max_length=1000)
     sub_category = models.ManyToManyField(SubCategory, related_name='cases', null=True)
     updated_at = models.DateTimeField(auto_now=False, null=True, blank=True)
-    vulnerabilites = models.TextField(max_length=200, null=True, blank=True)
+    vulnerabilites = models.CharField(max_length=200, null=True, blank=True)
 
 
 class CaseReferall(models.Model):
--- location_management/models.py
+++ location_management/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Province(models.Model):
-    name = models.TextField(max_length=25)
+    name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
@@ -10,7 +10,7 @@ class Province(models.Model):
 
 
 class District(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
     province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='province')
 
     def __str__(self):
@@ -18,7 +18,7 @@ class District(models.Model):
 
 
 class Location(models.Model):
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
     district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='location')
 
     def __str__(self):
@@ -26,7 +26,7 @@ class Location(models.Model):
 
 
 class Community(models.Model):
-    name = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='community')
 
     def __str__(self):
