a4b7eadef954340d9d7bd134b681a1d6b1a2d55b
Vasco Xavier <vliscoxavier06@gmail.com>
2020-03-02 16:29:10 +0200

    replace jwt rest for oauth2

--- callcenter/configuration/settings_production.py
+++ callcenter/configuration/settings_production.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
