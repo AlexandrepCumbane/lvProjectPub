5374d1db2d782e48cac7a9e7f9ac9a21807de495
Vasco Xavier <vliscoxavier06@gmail.com>
2020-03-02 16:17:21 +0200

    configure manager.py to run local settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callcenter.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callcenter.settings_local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
