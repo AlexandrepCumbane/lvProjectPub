f118d4f9219234c2cbc39ad83b50f4485015a04b
Vasco Xavier <vliscoxavier06@gmail.com>
2020-03-02 16:41:58 +0200

    remove env for manage.py

--- manage.py
+++ manage.py
@@ -5,10 +5,7 @@ import sys
 
 
 def main():
-    if os.getenv('DEBUG') is False:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callcenter.configuration.settings_production')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callcenter.settings_local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callcenter.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
