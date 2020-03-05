636c4ade0d09b437c11043fe298354f4944a9818
Vasco Xavier <vliscoxavier06@gmail.com>
2020-03-05 17:34:25 +0200

    update serializer

--- case_manager/api/serializers.py
+++ case_manager/api/serializers.py
@@ -165,6 +165,7 @@ class CaseSerializerFull(ModelSerializer):
     sub_category = SubCategorySerializer(many=True)
     created_by = serializer_factory(model=User, fields=('id','username',))()
     how_knows_us = HowDoYouHearAboutUsSerializer()
+    how_would_you_like_to_be_contacted = HowWouldYouLikeToBeContactedSerializer()
     case_status = serializer_factory(model=CaseStatus, fields=('id','name'))()
     programme = ProgrammeSerializer()
 
