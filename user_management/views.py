c58905c74508ec7d76751d93a39cde965380e0fb
Vasco Xavier <vliscoxavier06@gmail.com>
2020-03-06 14:00:19 +0200

    add backend permission

--- user_management/views.py
+++ user_management/views.py
@@ -1,5 +1,8 @@
 import requests
 
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+
 from django.http.response import JsonResponse
 
 from oauth2_provider.models import Application
@@ -17,9 +20,10 @@ def generate_token(request):
         dos metodos padroes de modo a asseguar que o client_secret e
         o client_id nao fossem diretamente escritos na app front-end
     """
+    username = request.data['username']
     # Filtro da aplicacao do cliente na base de dados
     # de modo a obter o client ID e o cliente secret
-    application = Application.objects.filter(user__username=request.data['username']).first()
+    application = Application.objects.filter(user__username=username).first()
 
     if application is None:
         # Se nao tem uma app registrada esse utilizador(caso exista)
@@ -44,6 +48,26 @@ def generate_token(request):
     response = requests.post(
                              addapter + request.get_host() + '/api/v1/o/token/',
                              data=login_data)
-    print('my response', response)
+    if response.status_code == 200:
+        my_response = response.json()
+        user = User.objects.get(username=username)
+        group_name = user.groups.first().name
+        return JsonResponse(set_user_pemission(my_response, group_name))
+
 
     return JsonResponse(response.json(), status=response.status_code)
+
+
+def set_user_pemission(data, group_name):
+    data['is_gestor'] = False
+    data['is_parceiro'] = False
+    data['is_operador'] = False
+
+    if group_name == 'Gestor':
+        data['is_gestor'] = True
+    elif group_name == 'Parceiro':
+        data['is_parceiro'] = True
+    elif group_name == 'Operador':
+        data['is_operador'] = True
+    
+    return data
