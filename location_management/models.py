2284ce228da4f14b00d9c2a943c968570b9c29a9
Vasco Xavier <vliscoxavier06@gmail.com>
2020-04-02 11:40:56 +0200

    add new fields to location

--- location_management/models.py
+++ location_management/models.py
@@ -8,10 +8,26 @@ class Province(models.Model):
         return self.name
 
 
+class LocationType(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class LocationClassification(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Location(models.Model):
+    classification = models.ForeignKey(LocationClassification, on_delete=models.SET_NULL, null=True, default=None)
+    codigo = models.CharField(max_length=20, default='')
     name = models.CharField(max_length=200)
-
+    province = models.ForeignKey(Province, on_delete=models.SET_NULL, null=True, default=None)
+    parent_code = models.CharField(max_length=20, default='')
 
     def __str__(self):
         return self.name
