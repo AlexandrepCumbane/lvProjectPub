38fa6f17ef170d07da33a4bf13a761857c84d1cc
Vasco Xavier <vliscoxavier06@gmail.com>
2020-04-06 15:41:06 +0200

    add import export to admin

--- location_management/admin.py
+++ location_management/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from location_management.models import Location
@@ -6,7 +7,10 @@ from location_management.models import LocationClassification
 from location_management.models import LocationType
 from location_management.models import Province
 
-admin.site.register(Location, list_display=('name',))
+@admin.register(Location)
+class LocationAdmin(ImportExportModelAdmin):
+    pass
+
 admin.site.register(Province, list_display=('name',))
 admin.site.register(LocationClassification)
 admin.site.register(LocationType)
