bb7f6ba6464291b51fbb54c45e0a2324f639a6aa
Vasco Xavier <vliscoxavier06@gmail.com>
2020-04-15 12:47:56 +0200

    refactoring code

--- case_manager/migrations/0044_auto_20200415_1000.py
+++ case_manager/migrations/0044_auto_20200415_1000.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('case_manager', '0043_auto_20200415_0748'),
+        ("case_manager", "0043_auto_20200415_0748"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='case',
-            old_name='resolution_callback',
-            new_name='call_require_aditional_information',
+            model_name="case",
+            old_name="resolution_callback",
+            new_name="call_require_aditional_information",
         ),
         migrations.AddField(
-            model_name='case',
-            name='call_require_callback_for_feedback',
+            model_name="case",
+            name="call_require_callback_for_feedback",
             field=models.BooleanField(default=False),
         ),
     ]
