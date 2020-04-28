19cf252ca7f1427a8435fa7550b573102a779e73
obadias <jr.pelembeobadias@gmail.com>
2020-04-28 10:54:06 +0200

    Adding PostLanguage to Api

--- posts_management/api/endpoints.py
+++ posts_management/api/endpoints.py
@@ -1,8 +1,9 @@
 from drf_auto_endpoint.router import DefaultRouter
 
-from posts_management.api.views import PostCategoriesViewSet, PostViewSet
+from posts_management.api.views import PostCategoriesViewSet, PostLanguageSerializer,PostViewSet
 
 router = DefaultRouter()
 
 router.register("posts", PostViewSet, basename="posts")
 router.register("post-categories", PostCategoriesViewSet, basename="post-categories")
+router.register("post-languages", PostCategoriesViewSet, basename="post-languages")
