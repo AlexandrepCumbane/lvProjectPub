f3ee6b124557eda0469787ae6d1154597ccbf945
obadias <jr.pelembeobadias@gmail.com>
2020-04-28 12:32:14 +0200

    post language viewset

--- posts_management/api/endpoints.py
+++ posts_management/api/endpoints.py
@@ -1,9 +1,9 @@
 from drf_auto_endpoint.router import DefaultRouter
 
-from posts_management.api.views import PostCategoriesViewSet, PostLanguageSerializer,PostViewSet
+from posts_management.api.views import PostCategoriesViewSet, PostLanguageViewSet,PostViewSet
 
 router = DefaultRouter()
 
 router.register("posts", PostViewSet, basename="posts")
 router.register("post-categories", PostCategoriesViewSet, basename="post-categories")
-router.register("post-languages", PostLanguageSerializer, basename="post-languages")
+router.register("post-languages", PostLanguageViewSet, basename="post-languages")
