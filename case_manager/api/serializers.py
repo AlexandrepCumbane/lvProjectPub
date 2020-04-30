e19db1397ae1534c853b1979bf052ee2004396a1
Vasco Xavier <vliscoxavier06@gmail.com>
2020-04-30 11:53:19 +0200

    add new fields to case task serializer

--- case_manager/api/serializers.py
+++ case_manager/api/serializers.py
@@ -251,6 +251,7 @@ class CaseSerializerTask(ModelSerializer):
 class CaseTaskFullSerializer(ModelSerializer):
 
     assigned_to = serializer_factory(model=User, fields=("id", "username"))()
+    updated_by = serializer_factory(model=User, fields=("id", "username"))()
     created_by = serializer_factory(model=User, fields=("id", "username"))()
     status = TaskStatusSerializer()
     case = serializer_factory(model=Case, fields=("case_id", "id"))()
