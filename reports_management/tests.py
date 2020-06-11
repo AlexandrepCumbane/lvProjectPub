c3b844c140bba31f1b7664ca96e59a1381aef567
Vasco Xavier <vliscoxavier06@gmail.com>
2020-06-11 19:56:35 +0200

    right unit tests for reports app

--- reports_management/tests.py
+++ reports_management/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User, Group
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+
+
+class UserReportTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="vasco", email="vasco.xavier@robobo.org", password="Test123."
+        )
+
+        user.save()
+        user.set_password(user.password)
+        user.save()
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+
+    def test_can_list_user_general_reports(self):
+        response = self.client.get("/api/v1/general-reports/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_list_user_dashboard_reports(self):
+        response = self.client.get("/api/v1/dashboard-reports/")
+        self.assertEqual(response.status_code, 200)
