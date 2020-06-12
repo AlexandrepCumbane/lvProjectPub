f622553a235c7b6f38fa60713bc7db908303ddef
Vasco Xavier <vliscoxavier06@gmail.com>
2020-06-12 12:28:08 +0200

    add docstring for the functions in the user_managements app test cases

--- user_management/tests.py
+++ user_management/tests.py
@@ -34,6 +34,9 @@ class UserLoginTestCase(TestCase):
         user.save()
 
     def test_user_can_generate_token(self):
+        """
+        Verify if the credentials of a user are valid and generate token.
+        """
         user = User.objects.first()
         data = {
             "username": user.username,
@@ -46,12 +49,20 @@ class UserLoginTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_user_exists(self):
+        """
+        Test if the user given a email exists in the database.
+        """
         my_email = "vasco.xavier@robobo.org"
         user = check_user_exists(my_email)
 
         self.assertIsNotNone(user)
 
     def test_check_user_is_gestor(self):
+        """
+        Check if the user belongs to the group of Gestor,
+        The funcion get_user_permission can 
+        set the is_gestor atributo to True.
+        """
         user = User.objects.first()
         gestor = Group.objects.get(name="Gestor")
         gestor.user_set.add(user)
@@ -61,6 +72,11 @@ class UserLoginTestCase(TestCase):
         self.assertTrue(new_user["is_gestor"])
 
     def test_check_user_is_operador(self):
+        """
+        Check if the user belongs to the group of Operador,
+        The funcion get_user_permission can 
+        set the is_operador atributo to True.
+        """
         user = User.objects.first()
         operador = Group.objects.get(name="Operador")
         operador.user_set.add(user)
@@ -70,6 +86,11 @@ class UserLoginTestCase(TestCase):
         self.assertTrue(new_user["is_operador"])
 
     def test_check_user_is_parceiro(self):
+        """
+        Check if the user belongs to the group of Parceiro,
+        The funcion get_user_permission can 
+        set the is_parceiro atributo to True.
+        """
         user = User.objects.first()
         parceiro = Group.objects.get(name="Parceiro")
         parceiro.user_set.add(user)
@@ -79,6 +100,11 @@ class UserLoginTestCase(TestCase):
         self.assertTrue(new_user["is_parceiro"])
 
     def test_check_user_is_ponto_focal(self):
+        """
+        Check if the user belongs to the group of Ponto Focal,
+        The funcion get_user_permission can 
+        set the is_ponto_focal atributo to True.
+        """
         user = User.objects.first()
         ponto_focal = Group.objects.get(name="Ponto Focal")
         ponto_focal.user_set.add(user)
@@ -106,6 +132,10 @@ class UserAPITestCase(TestCase):
         self.client.force_authenticate(user=user_me)
 
     def test_create_user_from_api(self):
+        """
+        Test if its possible to create user from the users
+        endpoint API.
+        """
         user = {
             "username": "quinho",
             "password": "Test123.",
@@ -119,7 +149,10 @@ class UserAPITestCase(TestCase):
         self.assertEquals(response.status_code, 200)
 
     def test_cant_create_user_without_username(self):
-
+        """
+        Test if its not possible to create user without
+        username.
+        """
         user = {
             "password": "Test123.",
             "first_name": "vasco",
@@ -134,7 +167,10 @@ class UserAPITestCase(TestCase):
         self.assertEquals(response.status_code, 400)
 
     def test_edit_user_email(self):
-
+        """
+        Test it its possible to edi user email from
+        the users endpoints API.
+        """
         user = {
             "username": "quinho",
             "password": "Test123.",
@@ -154,5 +190,9 @@ class UserAPITestCase(TestCase):
         self.assertEquals(response.status_code, 200)
 
     def test_list_users(self):
+        """
+        Test if its possible to read the list of users
+        from the users endpoint API.
+        """
         response = self.client.get("/api/v1/users/")
         self.assertEquals(response.status_code, 200)
