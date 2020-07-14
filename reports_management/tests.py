112cbdd88803f8b6c26cff25e56a90b05cabc8d7
Vasco Xavier <vliscoxavier06@gmail.com>
2020-07-14 11:46:31 +0200

    Add functions to test if its possible to generate parceiro and ponto focal dashboard data

--- reports_management/tests.py
+++ reports_management/tests.py
@@ -3,12 +3,24 @@ from django.test import TestCase
 
 from rest_framework.test import APIClient
 
+from reports_management.api.helpers import get_parceiro_dashboard_data, generate_focal_point_dashboard_data
 
 class UserReportTestCase(TestCase):
     def setUp(self):
         user = User.objects.create(
             username="vasco", email="vasco.xavier@robobo.org", password="Test123."
         )
+
+        group = Group.objects.create(name='Parceiro')
+        group.save()
+
+        group = Group.objects.create(name='Operador')
+        group.save()
+
+        group = Group.objects.create(name='Ponto Focal')
+        group.save()
+
+        group = Group.objects.create(name='Gestor')
 
         user.save()
         user.set_password(user.password)
@@ -31,3 +43,60 @@ class UserReportTestCase(TestCase):
         """
         response = self.client.get("/api/v1/dashboard-reports/")
         self.assertEqual(response.status_code, 200)
+    
+    def test_can_generate_parceiro_dashboard_data(self):
+        """
+        Test if its possible to generate parceiro dashboard
+        data.
+        """
+        parceiro_group = Group.objects.get(name='Parceiro')
+        user = User.objects.first()
+
+        parceiro_group.user_set.add(user)
+        result = get_parceiro_dashboard_data(user)
+
+        parceiro_data = {
+            "total_cases_received": 0,
+            "total_cases_with_feedback": 0,
+            "total_cases_rejected": 0,
+            "total_cases": 0,
+        }
+        self.assertDictEqual(result, parceiro_data)
+    
+    def test_can_generate_ponto_focal_dashboard_data(self):
+        """
+        Test if its possible to generate ponto focal dashboard
+        data.
+        """
+        ponto_focal_group = Group.objects.get(name='Ponto Focal')
+        user = User.objects.first()
+
+        ponto_focal_group.user_set.add(user)
+        result = generate_focal_point_dashboard_data(user)
+
+        ponto_focal_data = {
+            "total_cases_received": 0,
+            "total_cases_send": 0,
+            "total_cases_with_feedback": 0,
+            "total_cases": 0,
+        }
+        self.assertDictEqual(result, ponto_focal_data)
+
+    def test_can_get_ponto_focal_dashboard_data(self):
+        """
+        Test if its possible to get ponto focal dashboard
+        data.
+        """
+        ponto_focal_group = Group.objects.get(name='Ponto Focal')
+        user = User.objects.first()
+
+        ponto_focal_group.user_set.add(user)
+        result = get_parceiro_dashboard_data(user)
+
+        ponto_focal_data = {
+            "total_cases_received": 0,
+            "total_cases_send": 0,
+            "total_cases_with_feedback": 0,
+            "total_cases": 0,
+        }
+        self.assertDictEqual(result, ponto_focal_data)
