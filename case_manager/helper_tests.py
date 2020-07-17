eb3de9327d3af1cd556f83983d5a72a7a51ede90
Vasco Xavier <vliscoxavier06@gmail.com>
2020-07-17 22:30:29 +0200

    Add helpers setup to case_manager tests

--- case_manager/helper_tests.py
+++ case_manager/helper_tests.py
@@ -1,35 +1,32 @@
 from case_manager.models import (
     Ages,
-    Case,
-    CaseComments,
     CasePriority,
-    CaseReferall,
     CaseStatus,
-    CaseTask,
     Category,
     CategoryIssue,
-    Contactor,
     CustomerSatisfaction,
     Gender,
-    HowCaseClose,
     HowDoYouHearAboutUs,
     HowWouldYouLikeToBeContacted,
     IndividualCommitedFraud,
     MecanismUsed,
     Programme,
     ReferallEntity,
-    ResolutionCategory,
-    ResolutionSubCategory,
-    ResponseProgram,
     SourceOfInformation,
     SubCategory,
     TaskStatus,
     TransfereModality,
     Vulnerability,
-    WhoIsNotReceivingAssistence,
 )
 
+from location_management.models import District, Province
+
+
 def generate_initial_tests_data():
+    """
+        This function create all models nescessary to make requests
+        to case API.
+    """
     ages = Ages.objects.create(name="17 and below")
     ages.save()
 
@@ -38,7 +35,7 @@ def generate_initial_tests_data():
 
     case_status = CaseStatus.objects.create(name="Not Started")
     case_status.save()
-    
+
     case_status = CaseStatus.objects.create(name="Closed")
     case_status.save()
 
@@ -48,23 +45,32 @@ def generate_initial_tests_data():
     category = Category.objects.create(name="Complaint/ Negative feedback")
     category.save()
 
-    category_issue = CategoryIssue.objects.create(name="Fraud", category=category)
-    category_issue.save()
-
     customer_satisfaction = CustomerSatisfaction.objects.create(name="Happy")
     customer_satisfaction.save()
 
     gender = Gender.objects.create(name="Masculino")
     gender.save()
 
-    how_do_your_hear_about_us = HowWouldYouLikeToBeContacted.objects.create(name="Radio")
+    how_do_your_hear_about_us = HowWouldYouLikeToBeContacted.objects.create(
+        name="Radio"
+    )
     how_do_your_hear_about_us.save()
 
-    how_would_you_like_to_be_contacteds = HowWouldYouLikeToBeContacted.objects.create(name="Other phone")
+    how_would_you_like_to_be_contacteds = HowWouldYouLikeToBeContacted.objects.create(
+        name="Other phone"
+    )
     how_would_you_like_to_be_contacteds.save()
 
-    sub_category = SubCategory.objects.create(name="Humanitarian Actor")
+    sub_category = SubCategory.objects.create(name="Fraud", category=category)
     sub_category.save()
+
+    category_issue = CategoryIssue.objects.create(name="Fraud", category=sub_category)
+    category_issue.save()
+
+    indivivdual_commit_fraud = IndividualCommitedFraud.objects.create(
+        name="Local Leader", sub_category=sub_category
+    )
+    indivivdual_commit_fraud.save()
 
     mecanism_used = MecanismUsed.objects.create(name="Email")
     mecanism_used.save()
@@ -75,7 +81,9 @@ def generate_initial_tests_data():
     partner = ReferallEntity.objects.create(name="Parceiro")
     partner.save()
 
-    source_of_information = SourceOfInformation.objects.create(name="Humanitarion partner")
+    source_of_information = SourceOfInformation.objects.create(
+        name="Humanitarion partner"
+    )
     source_of_information.save()
 
     task_status = TaskStatus.objects.create(name="Not Started")
@@ -87,4 +95,10 @@ def generate_initial_tests_data():
     vulnerability = Vulnerability.objects.create(name="Single parent")
     vulnerability.save()
 
-    
+    province = Province.objects.create(name="Maputo")
+    province.save()
+
+    district = District.objects.create(
+        name="Bilene", province=province, codigo="4343", parent_code="4232"
+    )
+    district.save()
