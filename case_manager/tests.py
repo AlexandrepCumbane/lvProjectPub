13ba310f2d5f90e291227194309245846e983517
Vasco Xavier <vliscoxavier06@gmail.com>
2020-07-17 22:31:25 +0200

    add tests to Contactor functions on CaseViewset

--- case_manager/tests.py
+++ case_manager/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from case_manager.api.views import CaseViewset
 
 from case_manager.models import (
     Ages,
@@ -32,7 +33,77 @@ from case_manager.models import (
     WhoIsNotReceivingAssistence,
 )
 
+from case_manager.helper_tests import generate_initial_tests_data
 
-class CaseTestCase(TestCase):
-    def test_save_contactor(self):
-        pass
+from location_management.models import Location, District, Province
+
+
+class ContactorTestCase(TestCase):
+    def setUp(self):
+        generate_initial_tests_data()
+
+        contactor = Contactor.objects.create(gender=Gender.objects.first())
+        contactor.save()
+
+    def test_save_contactor_with_gender_only(self):
+        case_view = CaseViewset()
+
+        contactor = {
+            "gender": Gender.objects.first().id,
+        }
+        contactor_saved = case_view._save_contactor(contactor)
+
+        self.assertTrue(contactor_saved["is_saved"])
+
+    def test_save_contactor_full(self):
+        case_view = CaseViewset()
+
+        contactor = {
+            "gender": Gender.objects.first().id,
+            "alternative_number": "",
+            "contact": "",
+            "fdp": "",
+            "full_name": "",
+            "age": Ages.objects.first().id,
+            "community": "",
+            "district": None,
+            "province": Province.objects.first().id,
+            "localtion": "",
+        }
+        contactor_saved = case_view._save_contactor(contactor)
+
+        self.assertTrue(contactor_saved["is_saved"])
+
+    def test_cant_save_contactor_without_gender(self):
+        case_view = CaseViewset()
+
+        contactor = {
+            "alternative_number": "",
+            "contact": "",
+            "fdp": "",
+            "full_name": "",
+            "age": Ages.objects.first().id,
+            "community": "",
+            "district": None,
+            "province": Province.objects.first().id,
+            "localtion": "",
+        }
+        contactor_saved = case_view._save_contactor(contactor)
+
+        self.assertFalse(contactor_saved["is_saved"])
+
+    def test_cant_update_contactor(self):
+        case_view = CaseViewset()
+        contactor = {}
+        contactor_saved = case_view._update_contactor(contactor)
+        self.assertFalse(contactor_saved)
+
+    def test_can_update_contactor_only_with_id(self):
+        case_view = CaseViewset()
+        contactor = {
+            "id": Contactor.objects.first().id,
+            "full_name": "Vasco Xavier",
+            "gender": Gender.objects.first().id,
+        }
+        contactor_saved = case_view._update_contactor(contactor)
+        self.assertTrue(contactor_saved)
