8d6df53f4202383533aeb069feb73cbcc0c8432a
Vasco Xavier <vliscoxavier06@gmail.com>
2020-07-23 11:18:35 +0200

    redirect to admin when user go to domain root

--- callcenter/urls.py
+++ callcenter/urls.py
@@ -1,32 +1,18 @@
-"""callcenter URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from rest_framework.documentation import include_docs_urls
 from rest_framework_jwt.views import obtain_jwt_token
-
 from user_management.views import generate_token
 
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", lambda request: redirect('/admin')),
     path("api/v1/", include("callcenter.api_urls")),
     path("api/v1/o/login/", generate_token),
     path("api/v1/o/token/", obtain_jwt_token),
     path("admin/", admin.site.urls),
-    path("docs/", include_docs_urls("Recrutamento API")),
+    path("docs/", include_docs_urls("LinhaVerde API")),
     re_path(r"^ckeditor/", include("ckeditor_uploader.urls")),
 ]
