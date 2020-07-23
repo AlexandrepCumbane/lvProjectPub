de37114ae81f345f2a81aecfb6245d5abffb8435
Vasco Xavier <vliscoxavier06@gmail.com>
2020-07-23 11:30:54 +0200

    add initial location management and case manager docstring for the module

---
+++ case_manager/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Help on case_manager module on the application.
+
+    DESCRIPTION
+        This module contains a serie of Views and Models responsibles
+        to store the information about the cases in the System.
+"""
---
+++ location_management/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Help on location_managements module on the application.
+
+    DESCRIPTION
+        This module contains a serie of Views and Models related to
+        cases locations on the systems such as Province, Localities, Districts, etc.
+"""
