2bd316b3e7cf6e4475c0a4ea86503bec149a1219
Vasco Xavier <vliscoxavier06@gmail.com>
2020-09-24 11:17:00 +0200

    configure django django- nose and text coverage in project settings

--- callcenter/settings.py
+++ callcenter/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     # Checkeditor
     "ckeditor",
     "ckeditor_uploader",
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,13 @@ JWT_AUTH = {
     "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
     "JWT_AUTH_COOKIE": None,
 }
+
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=case_manager,location_management,posts_management,user_management',
+]
