07451c15b5993e356731d1716f8cc60c1ed2b626
Feisung_robobo <feimanheche@robobo.org>
2020-11-30 11:11:08 +0200

    Reorganized and test for app that contains vuexy theme

--- app/src/index.js
+++ app/src/index.js
@@ -1,38 +1,62 @@
+import React, { Suspense, lazy } from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { Layout } from "./utility/context/Layout"
+import * as serviceWorker from "./serviceWorker"
+import { store } from "./redux/storeConfig/store"
+import Spinner from "./components/@vuexy/spinner/Fallback-spinner"
+import "./index.scss"
+import "./@fake-db"
 import app, { patterns, photos } from '@wq/app';
 import map from '@wq/map';
 import config from './config';
-import * as serviceWorker from './serviceWorker';
+
+const LazyApp = lazy(() => import("./App"))
 
 app.use(map);
 app.use(patterns);
 app.use(photos);
+
 app.use({
-    context() {
-        const { version } = config;
-        return { version };
-    },
-    run($page) {
-        $page.find('form[data-wq-confirm]').on('submit', () => {
-            return app.confirmSubmit(this, 'Are you sure you want to delete this record?');
-        });
-        $page.find('button[data-wq-action="sync"]').on('click', () => {
-            app.retryAll();
-        });
-        $page.find('button[data-wq-action="empty-outbox"]').on('click', () => {
-            app.emptyOutbox(true);
-        });
-    }
+  context() {
+      const { version } = config;
+      return { version };
+  },
+  run($page) {
+      $page.find('form[data-wq-confirm]').on('submit', () => {
+          return app.confirmSubmit(this, 'Are you sure you want to delete this record?');
+      });
+      $page.find('button[data-wq-action="sync"]').on('click', () => {
+          app.retryAll();
+      });
+      $page.find('button[data-wq-action="empty-outbox"]').on('click', () => {
+          app.emptyOutbox(true);
+      });
+  }
 });
 
 var ready = app.init(config).then(function() {
-    app.jqmInit();
-    app.prefetchAll();
+  app.jqmInit();
+  app.prefetchAll();
 });
+
+// configureDatabase()
+ReactDOM.render(
+    <Provider store={store}>
+      <Suspense fallback={<Spinner />}>
+        <Layout>
+            <LazyApp />
+        </Layout>
+      </Suspense>
+    </Provider>,
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
+
 
 if (config.debug) {
     window.wq = app;
