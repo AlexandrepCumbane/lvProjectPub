c9306269a1d4857e87541f241947211a06c900ec
Fei Manheche <feimanheche@robobo.org>
2020-12-03 19:23:10 +0200

    Updated serializers to enable individual model submission

--- db/lv_form/rest.py
+++ db/lv_form/rest.py
@@ -1,10 +1,28 @@
 from wq.db import rest
-from .models import LvForm
-from .serializers import LvFormSerializer
+from .models import LvForm, CaseComment, ForwardingInstitution, Task
+from .serializers import LvFormSerializer, CaseCommentSerializer, ForwardingInstitutionSerializer, TaskSerializer
 
 
 rest.router.register_model(
     LvForm,
     serializer=LvFormSerializer,
+    fields="__all__",
+)
+
+rest.router.register_model(
+    CaseComment,
+    serializer=CaseCommentSerializer,
+    fields="__all__",
+)
+
+rest.router.register_model(
+    ForwardingInstitution,
+    serializer=ForwardingInstitutionSerializer,
+    fields="__all__",
+)
+
+rest.router.register_model(
+    Task,
+    serializer=TaskSerializer,
     fields="__all__",
 )
