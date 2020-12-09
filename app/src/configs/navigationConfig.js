6ff27e809e34e898baa5975e4efe75b83edf5af2
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2020-12-09 16:21:24 +0200

    Listing all views

--- app/src/configs/navigationConfig.js
+++ app/src/configs/navigationConfig.js
@@ -1,22 +1,45 @@
 import React from "react";
 import * as Icon from "react-feather";
-const navigationConfig = [
-  {
-    id: "home",
-    title: "Home",
-    type: "item",
-    icon: <Icon.Home size={20} />,
-    permissions: ["admin", "editor"],
-    navLink: "/",
-  },
-  {
-    id: "Cases",
-    title: "Lv Forms",
-    type: "item",
-    icon: <Icon.File size={20} />,
-    permissions: ["admin", "editor"],
-    navLink: "/lvforms",
-  },
-];
+
+import { default as config } from "../data/config";
+
+const get_pages = () => {
+  let navConf = [
+    {
+      id: "home",
+      title: "Home",
+      type: "item",
+      icon: <Icon.Home size={20} />,
+      permissions: ["admin", "editor"],
+      navLink: "/",
+    },
+    {
+      id: "Cases",
+      title: "Lv Forms",
+      type: "item",
+      icon: <Icon.File size={20} />,
+      permissions: ["admin", "editor"],
+      navLink: "/lvforms",
+    },
+  ];
+
+  const { pages } = config;
+
+  const urls = Object.keys(pages).map(function (key, index) {
+    return {
+      id: pages[key].name,
+      title: pages[key].name,
+      type: "item",
+      icon: <Icon.File size={20} />,
+      permissions: ["admin", "editor"],
+      navLink: pages[key].url,
+    };
+  });
+
+  console.log(urls);
+  return urls;
+};
+
+let navigationConfig = get_pages();
 
 export default navigationConfig;
