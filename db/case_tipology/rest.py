c55256dc81d69eb2e401b67735bf3ba490190395
Fei Manheche <feimanheche@robobo.org>
2020-12-14 14:45:08 +0200

    Added missing rest models to be picked up by swagger - fixes error 500

--- db/case_tipology/rest.py
+++ db/case_tipology/rest.py
@@ -1,10 +1,22 @@
 from wq.db import rest
-from .models import CaseTipology
-from .serializers import CaseTipologySerializer
+from .models import CaseTipology, SubCategory, SubCategoryIssue
+from .serializers import CaseTipologySerializer, SubCategorySerializer, SubCategoryIssueSerializer
 
 
 rest.router.register_model(
     CaseTipology,
     serializer=CaseTipologySerializer,
+    fields="__all__",
+)
+
+rest.router.register_model(
+    SubCategory,
+    serializer=SubCategorySerializer,
+    fields="__all__",
+)
+
+rest.router.register_model(
+    SubCategoryIssue,
+    serializer=SubCategoryIssueSerializer,
     fields="__all__",
 )
