c2911e0f95d58ea3cea7d5e9486907f0b65045e2
Fei Manheche <feimanheche@robobo.org>
2020-12-18 08:28:40 +0200

    Show and manage User groups in admin

--- db/accounts/admin.py
+++ db/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 from accounts.models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -13,7 +14,9 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
