0e45cf2806635a4cbc703ca60e8c50661d4a07f4
Feisung_robobo <feimanheche@robobo.org>
2021-01-06 09:15:36 +0200

    Integrate Auth0 auth

--- app/src/index.js
+++ app/src/index.js
@@ -10,21 +10,32 @@ import "./@fake-db";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { IntlProviderWrapper, LOCALES } from "./i18n/index";
+
+import { Auth0Provider } from "./authServices/auth0/auth0Service"
+import config from "./authServices/auth0/auth0Config.json"
+
 const LazyApp = lazy(() => import("./App"));
 
 const locale = LOCALES.ENGLISH;
 ReactDOM.render(
-  <Provider store={store}>
-    <Suspense fallback={<Spinner />}>
-      <PersistGate loading={null} persistor={persistor}>
-        <IntlProviderWrapper locale={locale}>
-          <Layout>
-            <LazyApp />
-          </Layout>
-        </IntlProviderWrapper>
-      </PersistGate>
-    </Suspense>
-  </Provider>,
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin + process.env.REACT_APP_PUBLIC_PATH}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <Provider store={store}>
+      <Suspense fallback={<Spinner />}>
+        <PersistGate loading={null} persistor={persistor}>
+          <IntlProviderWrapper locale={locale}>
+            <Layout>
+              <LazyApp />
+            </Layout>
+          </IntlProviderWrapper>
+        </PersistGate>
+      </Suspense>
+    </Provider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
 
