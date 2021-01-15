f93e04b750296abd5775918ffeccf18122e07f02
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-01-15 11:31:36 +0200

    Fixed admin path duplication

--- db/caseproject/urls.py
+++ db/caseproject/urls.py
@@ -46,8 +46,7 @@ urlpatterns = [
     re_path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', include(rest.router.urls)),
-    path('admin/', admin.site.urls),
+    path('', include(rest.router.urls)), 
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
