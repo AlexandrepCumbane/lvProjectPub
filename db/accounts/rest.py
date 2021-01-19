9a07de63f2ce276c54a414a84941cfedd0ff1abf
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-01-19 16:09:06 +0200

    Adding custom user viewset

--- db/accounts/rest.py
+++ db/accounts/rest.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from .models import CustomUser
 from .serializer import CustomUserSerializer
-
+from .views import UserViewSet
 # TODO: Filter to only return required fields
 rest.router.register_model(
     CustomUser,
     serializer=CustomUserSerializer,
     fields="__all__",
     cache="all",
+    viewset=UserViewSet,
 )
 
 rest.router.register_model(
--- db/accounts/views.py
+++ db/accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from wq.db.patterns import serializers
+
+from wq.db.rest.views import ModelViewSet
+
+from .models import CustomUser
+from .serializer import CustomUserSerializer
 
 # Create your views here.
+
+
+class UserViewSet(ModelViewSet):
+
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    @action(detail=True, methods=['get'])
+    def user_info(self, request, *args, **kwargs):
+        user = self.queryset.get(id=request.user.id)
+        serializer = self.serializer_class(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
