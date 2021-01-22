b18fa5c3f2a5ff89487e25c9ac997980b609abc6
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-01-22 12:42:48 +0200

    Apllying Role base access on task and lvform

--- db/lv_form/views.py
+++ db/lv_form/views.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 
 from wq.db.rest.views import ModelViewSet
 from accounts.serializer import CustomUserSerializer
-from .models import ForwardCaseToFocalpoint, ForwardingInstitution, LvForm
-from .serializers import LvFormSerializer
+from .models import ForwardCaseToFocalpoint, ForwardingInstitution, LvForm, Task
+from .serializers import LvFormSerializer, TaskSerializer
 
 # Create your views here.
 
@@ -44,6 +44,10 @@ class LvFormViewSet(ModelViewSet):
                 forwardinginstitution__in=fowarded_results)
 
             return lv_forms
+
+        if "operator" in user_data['groups_label']:
+
+            return self.queryset.filter(created_by__id=user.id)
 
         return self.queryset
 
@@ -94,5 +98,33 @@ class LvFormViewSet(ModelViewSet):
     @action(detail=True, methods=['get'])
     def fowarded_cases(self, request, *args, **kwargs):
         page = self.paginate_queryset(self.get_queryset_fowarded(request.user))
+        serializer = self.serializer_class(page, many=True)
+        return self.get_paginated_response(serializer.data)
+
+
+class TaskViewSet(ModelViewSet):
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get_queryset_list(self, user) -> list:
+        """
+            This method filter LvForms records from relative user groups/roles 
+        """
+
+        user_data = CustomUserSerializer(user).data
+
+        if "manager" in user_data['groups_label']:
+
+            return self.queryset
+
+        if "operator" in user_data['groups_label']:
+
+            return self.queryset.filter(assignee__id=user.id)
+
+        return self.queryset
+
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(self.get_queryset_list(request.user))
         serializer = self.serializer_class(page, many=True)
         return self.get_paginated_response(serializer.data)
