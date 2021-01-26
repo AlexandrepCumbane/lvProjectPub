62eeca23c468559b0df244ee16b5ba38032ab5b9
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-01-26 14:29:04 +0200

    Fixed timout reload on get token first time

--- app/src/redux/actions/app/helper.js
+++ app/src/redux/actions/app/helper.js
@@ -5,7 +5,7 @@ import { state } from "./forms";
 import { store } from "../../storeConfig/store";
 import { getUser } from "../auth/helper";
 
-const appState = store.getState();
+
 
 const authService = new AuthService();
 
@@ -18,6 +18,7 @@ export const handleForm = (dispatch, payload) =>
       loading: true,
     });
 
+    const appState = store.getState();
     const { userOauth } = appState.auth.login;
     axios
       .get(`/${payload.url}.json/`, {
@@ -61,6 +62,7 @@ export const handleForm = (dispatch, payload) =>
   });
 
 const requestSingle = (dispatch) => {
+  const appState = store.getState();
   let { dropdowns } = appState.app.app_reducer;
 
   state.map(async (item) => {
@@ -69,6 +71,7 @@ const requestSingle = (dispatch) => {
       item.name !== "login" &&
       item.url !== undefined
     ) {
+      
       const { userOauth } = appState.auth.login;
 
       if (dropdowns[item.name] === undefined) {
