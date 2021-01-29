ac04bbb70e3faae09f76d8fc5c498e6de3e38b6c
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-01-29 14:00:54 +0200

    Fixed save user

--- db/accounts/views.py
+++ db/accounts/views.py
@@ -68,10 +68,38 @@ class UserViewSet(ModelViewSet):
         if (user_serializer.is_valid()):
             user_serializer.save()
             user = CustomUser.objects.get(id=user_serializer.data['id'])
-            my_group = Group.objects.get(id=request.data['groups_id'])
-            my_group.user_set.add(user)
-            my_group.save()
+            self.add_to_group(request, user)
             return Response(user_serializer.data,
                             status=status.HTTP_201_CREATED)
         return Response(user_serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
+ 
+    
+    def update(self, request, *args, **kwargs):
+        instance = self.queryset.get(pk=kwargs.get('pk'))
+        serializer = CustomUserSerializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        user = CustomUser.objects.get(id=serializer.data['id'])
+        group = Group.objects.get(id=request.data['groups_id'])
+
+        user_data = self.serializer_class(user).data
+        if str(group.name) in user_data['groups_label']:
+            return Response(serializer.data)
+        else:
+            if len(user_data['groups_label']) > 0:
+                group = Group.objects.get(name=user_data['groups_label'][0])
+                group.user_set.remove(user)
+                group.save()
+
+                self.add_to_group(request, user)
+            else:
+                self.add_to_group(request, user)
+            return Response(serializer.data)
+
+    def add_to_group(self, request, user):
+        my_group = Group.objects.get(id=request.data['groups_id'])
+        my_group.user_set.add(user)
+        my_group.save()
+
