6561f6efb1412abed9fda4dc0624efcbed65c8a5
Leonardo Banze <leonardo.banze@robobo.org>
2021-02-02 15:14:26 +0200

    commenting component home and welcome in route.js

--- app/src/Router.js
+++ app/src/Router.js
@@ -11,12 +11,12 @@ import "./assets/scss/plugins/extensions/toastr.scss";
 
 import AppListView from "./views/app/list";
 // import { Home } from "react-feather";
-import Login from "./views/pages/authentication/login/Login";
+// import Login from "./views/pages/authentication/login/Login";
 
 // Route-based code splitting
 const Home = lazy(() => import("./views/pages/Home"));
 
-const Welcome = lazy(() => import("./views/pages/Welcome"));
+// const Welcome = lazy(() => import("./views/pages/Welcome"));
 
 // const Page2 = lazy(() => import("./views/pages/Page2"));
 
@@ -82,8 +82,8 @@ class AppRouter extends React.Component {
       // Set the directory path if you are deploying in sub-folder
       <Router history={history}>
         <Switch>
-          <AppRoute exact path="/" component={Login} fullLayout />
-          <AppRoute exact path="/welcome" component={Welcome} fullLayout />
+          {/* <AppRoute exact path="/" component={Login} fullLayout />
+          <AppRoute exact path="/welcome" component={Welcome} fullLayout /> */}
           <AppRoute exact path="/home" component={Home} />
           <AppRoute
             exact
