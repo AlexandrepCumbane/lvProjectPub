f50ae919e960d2d92afc6232f393c5f8fa939c21
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-03-16 11:08:14 +0200

    Adding blog filter

--- db/blog/rest.py
+++ db/blog/rest.py
@@ -1,10 +1,11 @@
 from wq.db import rest
 from .models import Article, Category
 from .serializers import ArticleSerializer
-
+from .views import ArticleViewSet
 rest.router.register_model(
     Article,
     serializer=ArticleSerializer,
+    viewset=ArticleViewSet,
     fields="__all__",
     cache="all",
 )
--- db/blog/views.py
+++ db/blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from rest_framework.decorators import action
+from wq.db.rest.views import ModelViewSet
 
-# Create your views here.
+from accounts.serializer import CustomUserFullSerializer
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleViewSet(ModelViewSet):
+
+    queryset = Article.objects.all().order_by('-id')
+    serializer_class = ArticleSerializer
+
+    def get_queryset_list(self, user) -> list:
+        """
+            This method filter LvForms records from relative user groups/roles 
+        """
+
+        user_data = CustomUserFullSerializer(user).data
+
+        if "manager" in user_data['groups_label']:
+
+            return self.queryset
+
+        if "operator" in user_data['groups_label']:
+
+            return self.queryset.filter(published=True)
+        else:
+
+            return []
+
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(self.get_queryset_list(request.user))
+        serializer = self.serializer_class(page, many=True)
+        return self.get_paginated_response(serializer.data)
