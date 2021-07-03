4170cb36caba4bf757970e38c861ef7c8ecc459c
Obadis Pelembe <jr.pelembeobadias@gmail.com>
2021-07-03 09:27:22 +0200

    Added notifications service on backend

--- db/notifications/models.py
+++ db/notifications/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Notification(models.Model):
-    title = models.CharField(max_length=250, unique=True)
+    model_id = models.CharField(max_length=15, unique=False, default="")
+    title = models.CharField(max_length=250, unique=False,
+    choices=(
+            ("New Case Comment", "New Case Comment"),
+            ("Case Forwarding", "Case Forwarding"),
+            ("New Task", "New Task"),
+            ("New Task Comment", "New Task Comment"),
+            ("Case Feedback", "Case Feedback"),
+            ("New Article", "New Article"),
+        ),)
     text = models.TextField(default="")
     
     is_deleted = models.BooleanField(
@@ -27,7 +36,7 @@ class Notification(models.Model):
         related_name="user_target",
     )
 
-    created_at = models.DateField(auto_now_add=True)
+    datetime_created = models.DateField(auto_now_add=True)
     created_by = models.ForeignKey(
         "accounts.CustomUser", on_delete=models.CASCADE, related_name="notification_creator"
     )
--- db/notifications/views.py
+++ db/notifications/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from rest_framework.decorators import action
+from wq.db.rest.views import ModelViewSet
 
-# Create your views here.
+from .models import Notification
+from .serializers import NotificationSerializer
+
+
+class NotificationViewSet(ModelViewSet):
+    queryset = Notification.objects.filter(watched=False).order_by('-id')
+    serializer_class = NotificationSerializer
+
+    def list(self, request, *args, **kwargs):
+        page = self.paginate_queryset(
+            self.queryset.filter(user_target=request.user))
+        serializer = self.serializer_class(page, many=True)
+        return self.get_paginated_response(serializer.data)
