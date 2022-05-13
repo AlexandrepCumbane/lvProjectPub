1a3417b8875f76b84d2733687d3e2a959a54421c
Fei Manheche <feimanheche@robobo.org>
2022-05-13 18:00:40 -0400

    - Handle mapping errors on certain fields - These should ultimately be handled by react app!

--- db/lv_form/utils.py
+++ db/lv_form/utils.py
@@ -4,9 +4,9 @@ from django.db import transaction
 from django.utils.datastructures import MultiValueDictKeyError
 
 from case_tipology.models import CaseTipology
-from location_management.models import Province
+from location_management.models import District
 from .serializers import LvFormSerializer
-from .models import LvForm
+from .models import CONTACT_GROUP_CHOICES, MEANS_OF_COMMUNICATION_CHOICES, HOW_KNOWS_LV_CHOICES, SECTOR_CHOICES, LvForm
 from .helper import mapped_value
 from django.contrib.auth import get_user_model
 
@@ -81,6 +81,18 @@ def map_relational_key(field, data, model, model_key, data_value_to_search):
             data[field] = model.objects.get(**{model_key:data[data_value_to_search]}).id
             return data
         else:
+            return data
+    except:
+        pass    # for now we continue without handling the error
+
+def map_choice_key(field, data, choice_model):
+    try:
+        if field in data:
+            for choice in choice_model:
+                if data[field].lower().startswith(choice[0].lower()):
+                    data[field] = choice[0]
+            return data
+        else:
             return data[field]
     except:
         pass    # for now we continue without handling the error
@@ -89,29 +101,46 @@ def map_case_fields(template_cases) -> dict:
     case_list = []
     User = get_user_model()
     keys_to_check = [
-                        #[key in response dict, DB Model, model_key, key to append/modify to response]
+                        #[key in response dict, DB Model, model_key, value to search on data dict]
                         # ['category_id', CaseTipology, 'category', 'category_id'],
-                        # ['distrito_id', Province, 'name', 'distrito_id'],
-                        ['created_by', User, 'email', 'created_by__label']
+                        ['distrito_id', District, 'name', 'distrito'],
+                        ['created_by', User, 'email', 'created_by__label'],
                     ]
+
+    choice_field_keys = [
+        ['contact_group', CONTACT_GROUP_CHOICES],
+        ['means_of_communication', MEANS_OF_COMMUNICATION_CHOICES],
+        ['how_knows_lv', HOW_KNOWS_LV_CHOICES],
+        ['sector', SECTOR_CHOICES],
+    ]
 
     serializer = {}
     with transaction.atomic():
         for i in range(len(template_cases)):
             # print(template_cases)
             # # add/map in missing items on form:
-            for key in keys_to_check:
-                map_relational_key(key[0], template_cases[i], key[1], key[2], key[3])
-
-            # Now write changes to the model
-            serializer = LvFormSerializer(data=template_cases[i], partial=True)
-            serializer.is_valid(raise_exception=True)
-
-            # We have to handle submiossions with empty user = defaults to null?
             try:
-                user = User.objects.get(id=template_cases[i]['created_by'])
-                serializer.save(created_by=user)
+                for key in keys_to_check:
+                    map_relational_key(key[0], template_cases[i], key[1], key[2], key[3])
+                
+                for key in choice_field_keys:
+                    map_choice_key(key[0], template_cases[i], key[1])
+
+                # Now write changes to the model
+                serializer = LvFormSerializer(data=template_cases[i], partial=True)
+                serializer.is_valid(raise_exception=True)
+
+                # We have to handle submiossions with empty user = defaults to null?
+                try:
+                    user = User.objects.get(id=template_cases[i]['created_by'])
+                    serializer.save(created_by=user)
+                except:
+                    serializer.save()
             except:
-                serializer.save()
+                # If a row fails for now we simply ignore and go to the next
+                # TODO: Handle array of error lines for feedback to front-end
+                # print(template_cases[i])
+                pass
+
 
     return serializer
