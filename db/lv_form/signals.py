ebcbbf552d6a1e0f4cac831e306558d992f9d7af
Alexandre Cumbane <alex_cumbane@hotmail.com>
2022-08-08 10:25:02 +0200

    verify existence case_number

--- db/lv_form/signals.py
+++ db/lv_form/signals.py
@@ -3,9 +3,14 @@ from django.db.models.signals import pre_save, post_save; # import the prev meth
 from .models import LvForm;
 
 
-def lvform_pre_save(*args, **kwargs):
+def lvform_pre_save(sender, instance, *args, **kwargs):
     print('lvform_pre_save')
-    print(args,kwargs)
+    print(sender,instance)
+    # verified if case_number existing
+    if instance.case_number is None:
+        print('caso duplicado')
+    else:
+        Print(instance.case_number)
 
 pre_save.connect(lvform_pre_save, sender=LvForm)
 
