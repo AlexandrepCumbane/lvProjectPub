092e0725bc577bde83e0082161da529cc75f49c5
Alexandre Cumbane <alex_cumbane@hotmail.com>
2022-08-09 08:42:42 +0200

    add new stap

--- db/lv_form/signals.py
+++ db/lv_form/signals.py
@@ -3,14 +3,30 @@ from django.db.models.signals import pre_save, post_save; # import the prev meth
 from .models import LvForm;
 
 
-def lvform_pre_save(sender, instance, *args, **kwargs):
+def lvform_pre_save(sender, instance,*args, **kwargs):
     print('lvform_pre_save')
-    print(sender,instance)
-    # verified if case_number existing
-    if instance.case_number is None:
-        print('caso duplicado')
+    # !gerar o numero de casos ( case_number)
+    numero = instance.case_number
+    # case_number_verifiy = LvForm.objects.get(case_number=numero)
+    # if case_number_verifiy:
+    #     print('Novo caso: '+ instance)
+    # else:
+    #     print('Caso: '+ instance)
+
+    if instance._state.adding:
+        print ('Instance created!')
+        print (instance)
     else:
-        Print(instance.case_number)
+        print ('Instance updated!')
+        print (instance)
+
+   
+
+    # ! verificar se este numero instanciado existe na base de dados;
+
+    #! verificar se esta sendo criado uma instancia de create ou de update
+ 
+        
 
 pre_save.connect(lvform_pre_save, sender=LvForm)
 
