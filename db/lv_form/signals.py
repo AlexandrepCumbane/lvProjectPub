ec51fcd227233f7c8aedf5ff49d27886b35fc32a
Alexandre Cumbane <alex_cumbane@hotmail.com>
2022-08-10 11:26:48 +0200

    concatinated casenumber to corrent year

--- db/lv_form/signals.py
+++ db/lv_form/signals.py
@@ -1,28 +1,19 @@
 from django.dispatch import receiver  # receiver are signals
 from django.db.models.signals import pre_save, post_save; # import the prev method sign
 from .models import LvForm;
+from datetime import date
 
 
 def lvform_pre_save(sender, instance,*args, **kwargs):
     print('lvform_pre_save')
-    # !gerar o numero de casos ( case_number)
-    instance.case_number = instance.uuid.time
-    print(instance.uuid.time)
-    if instance._state.adding:
-        print ('Instance created!')
-        print (instance)
-    else:
-        print ('Instance updated!')
-        print (instance)
 
-   
+    # get Current year 
+    current_year = date.today().year
 
-    # ! verificar se este numero instanciado existe na base de dados;
+    #generate uuid to case_number
+    instance.case_number = f'{current_year}{instance.uuid.time_mid}'
 
-    #! verificar se esta sendo criado uma instancia de create ou de update
- 
-        
-
+# load pre_save sign
 pre_save.connect(lvform_pre_save, sender=LvForm)
 
 
