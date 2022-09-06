bffcaa39d5bf4918ff1ae8dee40c875c48bc6e47
Odilia Nhacundela <odiliatina7@gmail.com>
2022-09-06 13:21:18 +0200

    Fix error message

--- app/src/views/app/modal/forwardCaseModal.js
+++ app/src/views/app/modal/forwardCaseModal.js
@@ -206,7 +206,7 @@ class Create extends React.Component {
     return (
       <Row>
         <Col md="12">
-          {this.state.isValid && this.state.f1 === "" ? (
+          {this.state.isValid && this.state.f1 ===! "" ? (
             <></>
           ) : (
             <Alert color="danger" className="square">
