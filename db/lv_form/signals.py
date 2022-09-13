63a39a4983dc91778f71cc0ce2df7a52b2ab7248
Alexandre Cumbane <alex_cumbane@hotmail.com>
2022-09-14 00:18:08 +0200

    prevent update case_number when update data

--- db/lv_form/signals.py
+++ db/lv_form/signals.py
@@ -1,27 +1,23 @@
-from asyncio.windows_events import NULL
 from django.dispatch import receiver  # receiver are signals
-from django.db.models.signals import pre_save  # import the prev method sign
-from .models import LvForm
+from django.db.models.signals import pre_save # import the prev method sign
+from .models import LvForm;
 from datetime import date
 
 
-def lvform_pre_save(sender, instance, *args, **kwargs):
+def lvform_pre_save(sender, instance,*args, **kwargs):
 
-    print(instance)
-    pass
-    if instance is None:
-
-        # get Current year
-        current_year = date.today().year
-
-        # generate uuid to case_number
-        instance.case_number = f'{current_year}{instance.uuid.time_mid}'
-
-        print("case number vazio")
-    if instance is not None:
+#! prevent update case_number when update data
+    if instance.case_number is None:
+        print("instancia vazia")
         print(instance)
-    #     print("Case number nao vazio")
-
+        # get Current year 
+        current_year = date.today().year
+        
+        #generate uuid to case_number
+        instance.case_number = f'{current_year}{instance.uuid.time_mid}'
 
 # load pre_save sign
 pre_save.connect(lvform_pre_save, sender=LvForm)
+
+
+
