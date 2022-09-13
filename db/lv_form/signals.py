e26cfe455467b6591b7f541ae0551321c1eea1cc
Odilia Nhacundela <odiliatina7@gmail.com>
2022-09-13 20:19:05 +0200

    1st step for resolucion of case_number bug.

--- db/lv_form/signals.py
+++ db/lv_form/signals.py
@@ -1,20 +1,27 @@
+from asyncio.windows_events import NULL
 from django.dispatch import receiver  # receiver are signals
-from django.db.models.signals import pre_save # import the prev method sign
-from .models import LvForm;
+from django.db.models.signals import pre_save  # import the prev method sign
+from .models import LvForm
 from datetime import date
 
 
-def lvform_pre_save(sender, instance,*args, **kwargs):
-    print('lvform_pre_save')
+def lvform_pre_save(sender, instance, *args, **kwargs):
 
-    # get Current year 
-    current_year = date.today().year
+    print(instance)
+    pass
+    if instance is None:
 
-    #generate uuid to case_number
-    instance.case_number = f'{current_year}{instance.uuid.time_mid}'
+        # get Current year
+        current_year = date.today().year
+
+        # generate uuid to case_number
+        instance.case_number = f'{current_year}{instance.uuid.time_mid}'
+
+        print("case number vazio")
+    if instance is not None:
+        print(instance)
+    #     print("Case number nao vazio")
+
 
 # load pre_save sign
 pre_save.connect(lvform_pre_save, sender=LvForm)
-
-
-
