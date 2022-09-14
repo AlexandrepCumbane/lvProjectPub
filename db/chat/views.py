9826c19f6a5cfed90866ad6dd23d36b531d088f0
Fei Manheche <feimanheche@robobo.org>
2022-09-14 04:29:51 -0400

    Remove erronous wss config from ws view

--- db/chat/views.py
+++ db/chat/views.py
@@ -6,12 +6,12 @@ def index(request):
 
 
 def room(request, room_name):
-    if request.is_secure():
-        # This is the production environment
-        ws_protocol = "wss://"
-    else:
-        # This is a localost or unsafe environment
-        ws_protocol = "ws://"
+    # if request.is_secure():
+    #     # This is the production environment
+    #     ws_protocol = "wss://"
+    # else:
+    #     # This is a localost or unsafe environment
+    ws_protocol = "ws://"
     
     return render(request, 'chat/room.html', {
         'room_name': room_name,
